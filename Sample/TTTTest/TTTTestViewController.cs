c66d49ba4bda9209c0a100ebcfb9f8d897496fba
Eric Fontana <eric@fontanas.net>
2014-03-02 09:57:51 -0500

    Added Sample App

---
+++ Sample/TTTTest/TTTTestViewController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using MonoTouch.TTTAttributedLabel;
+
+namespace TTTTest
+{
+  public partial class TTTTestViewController : UIViewController
+  {
+    static bool UserInterfaceIdiomIsPhone
+    {
+      get { return UIDevice.CurrentDevice.UserInterfaceIdiom == UIUserInterfaceIdiom.Phone; }
+    }
+
+    public TTTTestViewController()
+			: base(UserInterfaceIdiomIsPhone ? "TTTTestViewController_iPhone" : "TTTTestViewController_iPad", null)
+    {
+    }
+
+    public override void DidReceiveMemoryWarning()
+    {
+      // Releases the view if it doesn't have a superview.
+      base.DidReceiveMemoryWarning();
+			
+      // Release any cached data, images, etc that aren't in use.
+    }
+
+    public override void ViewDidLoad()
+    {
+      base.ViewDidLoad();
+      //
+      // TTTAttributedLabel label = new TTTAttributedLabel ();
+      label.Text = new NSString ("I love Tink");
+      label.AddLinkToURL (new NSUrl ("http://tinkapp.com/"), new NSRange (7, 4));
+      label.Delegate = new LabelDelegate();
+      label.BackgroundColor = UIColor.Cyan;
+
+      View.AddSubview(label);
+
+      // Perform any additional setup after loading the view, typically from a nib.
+    }
+  }
+
+  class LabelDelegate : TTTAttributedLabelDelegate 
+  {
+    public override void DidSelectLinkWithURL (TTTAttributedLabel label, NSUrl url)
+    {
+      Console.WriteLine ("Clicked URL: {0}", url.ToString ());
+    }
+  }
+}
+
---
+++ Sample/TTTTest/TTTTestViewController.designer.cs
@@ -0,0 +1,26 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
+
+namespace TTTTest
+{
+	[Register ("TTTTestViewController")]
+	partial class TTTTestViewController
+	{
+		[Outlet]
+		MonoTouch.TTTAttributedLabel.TTTAttributedLabel label { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (label != null) {
+				label.Dispose ();
+				label = null;
+			}
+		}
+	}
+}
