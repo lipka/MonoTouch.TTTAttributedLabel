53fb38e83cf5f79593f45cadd64be593d7f7bfae
Lukas Lipka <lukaslipka@gmail.com>
2014-05-06 15:28:03 +0200

    Update with LinkTarget.ArmV7s and frameworks.

--- binding/libTTTAttributedLabel.linkwith.cs
+++ binding/libTTTAttributedLabel.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libTTTAttributedLabel.a", LinkTarget.Simulator | LinkTarget.ArmV7, ForceLoad = true)]
+[assembly: LinkWith ("libTTTAttributedLabel.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, Frameworks = "CoreGraphics CoreText QuartzCore", ForceLoad = true)]
