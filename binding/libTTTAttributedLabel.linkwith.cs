b3e37f07743eb5243e23dfd946bca6e46d5663c4
Angel Anton <angel.anton@monday8am.com>
2015-01-15 18:47:00 +0100

    Support to Unified API

--- binding/libTTTAttributedLabel.linkwith.cs
+++ binding/libTTTAttributedLabel.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
-using MonoTouch.ObjCRuntime;
+using ObjCRuntime;
 
 [assembly: LinkWith ("libTTTAttributedLabel.a", LinkTarget.Simulator | LinkTarget.ArmV7 | LinkTarget.ArmV7s, Frameworks = "CoreGraphics CoreText QuartzCore", ForceLoad = true)]
